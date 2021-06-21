d175f32bf728ac7cbec1913dc98b4ea4f340636b
hoyer <hoyer@ifn.ing.tu-bs.de>
2021-06-21 22:08:02 +0200

    tidy up directory

---
+++ backend/5g-related/change_service/change_service.php
@@ -0,0 +1,12 @@
+<?php
+
+   /*  $zahl = 4;
+
+    function quadrat($zahl)
+    {
+        return $zahl * $zahl;
+    } */
+
+    // echo 'Das Quadrat von ' . $zahl . ' ist ' . quadrat($zahl) . '.';
+    echo 'Hello World!';
+?>
