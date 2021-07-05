bd308b0e5c4278019c420f81a1ebd4b7ebb89484
hoyer <hoyer@ifn.ing.tu-bs.de>
2021-07-05 16:22:23 +0200

    batch file added

--- backend/5g-related/change_service/change_service.bat
+++ backend/5g-related/change_service/change_service.bat
@@ -9,16 +9,20 @@ if %serviceNumber% == 1 (
     IF %serviceNumber% == 2 (
         set service=rtp://232.1.1.121:40021/video.mp4?pkt_size=1316
     ) ELSE (
-        echo "The service number is not 1 and not 2."
-        echo "The service number is "
-        echo %serviceNumber%
-        REM exit
+        echo The service number is not 1 and not 2.
+        echo The service number is %serviceNumber%.
+        GOTO:EOF
     )
 )
 
-start cmd /k cd /d C:\Users\hoyer\Videos ^&^& ffmpeg -re -stream_loop -1 -i movie.mp4 -c:v libx264 -f rtp_mpegts %service%
+start cmd /c cd /d C:\Users\hoyer\Videos ^&^& ffmpeg -re -stream_loop -1 -i movie.mp4 -c:v libx264 -f rtp_mpegts %service% &
+REM cd /d C:\Users\hoyer\Videos
+REM ffmpeg -re -stream_loop -1 -i movie.mp4 -c:v libx264 -f rtp_mpegts %service%
 
-REM start "" ffplay rtp://232.1.1.120:40020
+start cmd /c ffplay -probesize 32 -sync ext rtp://232.1.1.120:40020 &
 
-REM exit
+start cmd /c
+echo The service number is %serviceNumber%.
+
+exit
 
--- backend/5g-related/change_service/change_service.php
+++ backend/5g-related/change_service/change_service.php
@@ -1,20 +1,27 @@
 <?php
-    echo "Run change service script." . "<br>";
+    print "Run change service script." . "<br>";
+    flush();
 
     // Arguments
     if (PHP_SAPI === 'cli') {
         $Service_number = $argv[1];
     }
     else {
-        $Service_number = $_GET['service'];
+        if(isset($_GET['service'])) {
+            $Service_number = $_GET['service'];
+        } else {
+            echo "No service number has been provided as argument!" . "<br>";
+        }
     }
 
     $dir = getcwd();
+    /* echo $dir . "<br>";
+    echo "cmd /c START ". $dir . "\change_service.bat" . " " . $Service_number . " "; */
 
-    echo $dir . "<br>";
-
-    echo "cmd /c START ". $dir . "\change_service.bat" . " " . $Service_number . " ";
+    echo "Launch batch script!" . "<br>";
 
     exec("cmd /c START ". $dir . "\change_service.bat" . " " . $Service_number . " ");
     // system("cmd " . $dir . "\change_service.bat");
+
+    echo "Batch script ended!" . "<br>";
 ?>
